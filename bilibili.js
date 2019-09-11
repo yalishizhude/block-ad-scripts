a0d5a03ad6997dc9f83d4fdb0df85aaa985508b9
yalishizhude <yalishizhude@163.com>
2019-09-11 22:33:07 +0800

    bilibili

---
+++ bilibili.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         B站自动关闭弹幕
+// @version      0.1
+// @description  默认关闭弹幕
+// @author       https://yalishizhude.com/
+// @include      https://www.bilibili.com/video/*
+// @grant        none
+// @updateURL    https://github.com/yalishizhude/bilibili.js
+// @downloadURL  https://github.com/yalishizhude/bilibili.js
+// @supportURL   https://github.com/yalishizhude/block-ad-scripts/issues
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  const $ = (sel) => document.querySelector(sel)
+  const remove = () => {
+      if($('.bilibili-player-video-danmaku-switch.bui.bui-switch')) {
+          const mouseover = document.createEvent("MouseEvents");
+          mouseover.initMouseEvent("mouseover")
+          $('.bilibili-player-video-danmaku-switch.bui.bui-switch').dispatchEvent(mouseover)
+      }
+      if($('.choose_danmaku') && $('.choose_danmaku').textContent !== '开启弹幕') {
+          const danmu = $('.bui-checkbox')
+          danmu && danmu.click()
+          document.body.removeEventListener('DOMSubtreeModified', remove);
+      }
+    }
+  document.body.addEventListener('DOMSubtreeModified', remove);
+  remove()
+})();
