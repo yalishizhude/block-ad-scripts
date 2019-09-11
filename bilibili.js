324845ed54c19dcc60c50160881d7ddddc193c31
yalishizhude <yalishizhude@163.com>
2019-09-11 22:36:56 +0800

    version

--- bilibili.js
+++ bilibili.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         B站自动关闭弹幕
-// @version      0.1
+// @version      0.2
 // @description  默认关闭弹幕
 // @author       https://yalishizhude.com/
 // @include      https://www.bilibili.com/video/*
@@ -10,21 +10,21 @@
 // @supportURL   https://github.com/yalishizhude/block-ad-scripts/issues
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
   const $ = (sel) => document.querySelector(sel)
   const remove = () => {
-      if($('.bilibili-player-video-danmaku-switch.bui.bui-switch')) {
-          const mouseover = document.createEvent("MouseEvents");
-          mouseover.initMouseEvent("mouseover")
-          $('.bilibili-player-video-danmaku-switch.bui.bui-switch').dispatchEvent(mouseover)
-      }
-      if($('.choose_danmaku') && $('.choose_danmaku').textContent !== '开启弹幕') {
-          const danmu = $('.bui-checkbox')
-          danmu && danmu.click()
-          document.body.removeEventListener('DOMSubtreeModified', remove);
-      }
+    if ($('.bilibili-player-video-danmaku-switch.bui.bui-switch')) {
+      const mouseover = document.createEvent("MouseEvents");
+      mouseover.initMouseEvent("mouseover")
+      $('.bilibili-player-video-danmaku-switch.bui.bui-switch').dispatchEvent(mouseover)
     }
+    if ($('.choose_danmaku') && $('.choose_danmaku').textContent !== '开启弹幕') {
+      const danmu = $('.bui-checkbox')
+      danmu && danmu.click()
+      document.body.removeEventListener('DOMSubtreeModified', remove);
+    }
+  }
   document.body.addEventListener('DOMSubtreeModified', remove);
   remove()
 })();
