abdea25adf819139ffe72ae8cde3c681c5cca744
yalishizhude <yalishizhude@163.com>
2019-09-18 11:55:57 +0800

    remove baidu ad

---
+++ baidu.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         百度去广告
+// @version      1
+// @description  去除搜索广告
+// @author       https://yalishizhude.com/
+// @include      https://*baidu.com/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/yalishizhude/block-ad-scripts/master/baidu.js
+// @downloadURL  https://raw.githubusercontent.com/yalishizhude/block-ad-scripts/master/baidu.js
+// @supportURL   https://github.com/yalishizhude/block-ad-scripts/issues
+// ==/UserScript==
+
+(function() {
+  GM_addStyle(`
+    [cmatchid] {
+      height: 0;
+      overflow: hidden;
+    }
+  `)
+}())
