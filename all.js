320bff70dae856af71e6bf8d8b0395932e5b26b9
yalishizhude <yalishizhude@163.com>
2019-09-27 17:03:04 +0800

    tuiguang

---
+++ all.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         全局去广告
+// @version      1.1
+// @description  去除页面商品推广广告
+// @description  try to take over the world!
+// @author       https://yalishizhude.com/
+// @match        *://*/*
+// @webRequest [{"selector":"http://pos.baidu.com/*","action":"cancel"}]
+// @updateURL    https://raw.githubusercontent.com/yalishizhude/block-ad-scripts/master/all.js
+// @downloadURL  https://raw.githubusercontent.com/yalishizhude/block-ad-scripts/master/all.js
+// @supportURL   https://github.com/yalishizhude/block-ad-scripts/issues
+// ==/UserScript==
