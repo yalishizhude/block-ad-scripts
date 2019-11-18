0fb33bcbe5759c1219d0b9b4b511d0a5fb8a228e
yalishizhude <yalishizhude@163.com>
2019-11-19 00:23:36 +0800

    juejin

--- juejin.js
+++ juejin.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         掘金去广告
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://juejin.im/*
@@ -11,6 +11,6 @@
 
 (function() {
     'use strict';
-    GM_addStyle(`.ad-entry-list,.banner-block,.sticky-banner,.activity-recommend {display:none}`)
+    GM_addStyle(`.ad-entry-list,.banner-block,.sticky-banner,.activity-recommend,.recommended-area,.article-banner,.footer-author-block,.tag-list-box {display:none}`)
     // Your code here...
 })();
