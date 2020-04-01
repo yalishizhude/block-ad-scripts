4f37c3b4bf6c7d9ca7dae4af22e249c5a69dbda9
yalishizhude <yalishizhude@163.com>
2020-04-01 16:25:26 +0800

    zh

--- zhihu.js
+++ zhihu.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎去广告
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  去掉知乎广告以及页面标题
 // @author       亚里士朱德
 // @include      https://*.zhihu.com/*
@@ -14,38 +14,45 @@
 // @webRequest [{"selector":"https://www.zhihu.com/home_top.html","action":"cancel"}]
 // ==/UserScript==
 
-(function() {
-  'use strict';
-  GM_addStyle(`
-      .Pc-card.Card,.Pc-word, .TopstoryItem--advertCard,.QuestionHeader-title:first-child,.Card.QuestionInvitation {
-        display:none;
-      }
-      .Search-container [data-za-detail-view-path-module="AnswerItem"]:before,
-      .Search-container [data-za-detail-view-path-module="PostItem"]:before {
-        color: #fff;
-        padding: 1px 3px;
-        border-radius: 3px;
-        margin: 3px;
-        float: right;
-        display: none;
-      }
-      .Search-container [data-za-detail-view-path-module="PostItem"]:hover:before,
-      .Search-container [data-za-detail-view-path-module="AnswerItem"]:hover:before {
-        display: block;
-      }
-      .Search-container [data-za-detail-view-path-module="AnswerItem"] {
-        box-shadow: 0 0 1px 1px rgba(0, 150, 0, .2);
-      }
-      .Search-container [data-za-detail-view-path-module="AnswerItem"]:before {
-        content: '问答';
-        background-color: rgba(0, 150, 0);
-      }
-      .Search-container [data-za-detail-view-path-module="PostItem"] {
-        box-shadow: 0 0 1px 1px rgba(150, 0, 150, .2);
-      }
-      .Search-container [data-za-detail-view-path-module="PostItem"]:before {
-        content: '文章';
-        background-color: rgb(150, 0, 150);
-      }
-  `)
-})();
+GM_addStyle(`
+  .Pc-card.Card,.Pc-word, .TopstoryItem--advertCard,.QuestionHeader-title:first-child,.Card.QuestionInvitation {
+    display:none;
+  }
+  .Search-container [data-za-detail-view-path-module="AnswerItem"]:before,
+  .Search-container [data-za-detail-view-path-module="PostItem"]:before {
+    color: #fff;
+    padding: 1px 3px;
+    border-radius: 3px;
+    margin: 3px;
+    float: right;
+    display: none;
+  }
+  .Search-container [data-za-detail-view-path-module="PostItem"]:hover:before,
+  .Search-container [data-za-detail-view-path-module="AnswerItem"]:hover:before {
+    display: block;
+  }
+  .Search-container [data-za-detail-view-path-module="AnswerItem"] {
+    box-shadow: 0 0 1px 1px rgba(0, 150, 0, .2);
+  }
+  .Search-container [data-za-detail-view-path-module="AnswerItem"]:before {
+    content: '问答';
+    background-color: rgba(0, 150, 0);
+  }
+  .Search-container [data-za-detail-view-path-module="PostItem"] {
+    box-shadow: 0 0 1px 1px rgba(150, 0, 150, .2);
+  }
+  .Search-container [data-za-detail-view-path-module="PostItem"]:before {
+    content: '文章';
+    background-color: rgb(150, 0, 150);
+  }
+`)
+
+let title = 0
+const interval = setInterval(() => {
+  if (document.title === 'zhihu') title++
+  if (title > 3) {
+    clearInterval(interval)
+    title = 0
+  }
+  document.title = 'zhihu'
+}, 1000)
